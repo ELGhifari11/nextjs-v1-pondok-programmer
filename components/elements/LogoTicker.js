21b0f0a488c4606431c7ab34d76fcb011ec36dce
ELGhifari11 <ghifariakun@gmail.com>
2025-11-15 23:48:35 +0700

    refactor(LogoTicker): replace hardcoded logos with dynamic array mapping
    Use array of logo objects with src and alt attributes for better maintainability
    Add random sorting to shuffle logo display order

--- components/elements/LogoTicker.js
+++ components/elements/LogoTicker.js
@@ -4,41 +4,41 @@ import Marquee from 'react-fast-marquee'
 export default function LogoTicker() {
     return (
         <>
-                <Marquee
-                    // style={{ width: "auto" }}
-                    pauseOnHover={true}
-                    direction="left" className="carouselTicker__list list-logos">
-                    <li className="carouselTicker__item">
-                        <div className="item-logo"><img src="/assets/imgs/page/homepage3/logo1.png" alt="Nivia" /></div>
-                    </li>
-                    <li className="carouselTicker__item">
-                        <div className="item-logo"><img src="/assets/imgs/page/homepage3/logo2.png" alt="Nivia" /></div>
-                    </li>
-                    <li className="carouselTicker__item">
-                        <div className="item-logo"><img src="/assets/imgs/page/homepage3/logo3.png" alt="Nivia" /></div>
-                    </li>
-                    <li className="carouselTicker__item">
-                        <div className="item-logo"><img src="/assets/imgs/page/homepage3/logo4.png" alt="Nivia" /></div>
-                    </li>
-                    <li className="carouselTicker__item">
-                        <div className="item-logo"><img src="/assets/imgs/page/homepage3/logo5.png" alt="Nivia" /></div>
-                    </li>
-                    <li className="carouselTicker__item">
-                        <div className="item-logo"><img src="/assets/imgs/page/homepage3/logo6.png" alt="Nivia" /></div>
-                    </li>
-                    <li className="carouselTicker__item">
-                        <div className="item-logo"><img src="/assets/imgs/page/homepage3/logo7.png" alt="Nivia" /></div>
-                    </li>
-                    <li className="carouselTicker__item">
-                        <div className="item-logo"><img src="/assets/imgs/page/homepage3/logo8.png" alt="Nivia" /></div>
-                    </li>
-                    <li className="carouselTicker__item">
-                        <div className="item-logo"><img src="/assets/imgs/page/homepage3/logo9.png" alt="Nivia" /></div>
-                    </li>
-                    <li className="carouselTicker__item">
-                        <div className="item-logo"><img src="/assets/imgs/page/homepage3/logo1.png" alt="Nivia" /></div>
-                    </li>
-                </Marquee>
+            <Marquee
+                // style={{ width: "auto" }}    
+                pauseOnHover={true}
+                direction="left" className="carouselTicker__list list-logos">
+                {[
+                    { src: "/assets/imgs/page/homepage1/1.png", alt: "Hokben" },
+                    { src: "/assets/imgs/page/homepage1/2.png", alt: "..." },
+                    { src: "/assets/imgs/page/homepage1/3.png", alt: "..." },
+                    { src: "/assets/imgs/page/homepage1/4.png", alt: "..." },
+                    { src: "/assets/imgs/page/homepage1/5.png", alt: "..." },
+                    { src: "/assets/imgs/page/homepage1/7.png", alt: "..." },
+                    { src: "/assets/imgs/page/homepage1/8.png", alt: "..." },
+                    { src: "/assets/imgs/page/homepage1/9.png", alt: "..." },
+                    { src: "/assets/imgs/page/homepage1/10.png", alt: "..." },
+                    { src: "/assets/imgs/page/homepage1/11.png", alt: "..." },
+                    { src: "/assets/imgs/page/homepage1/12.png", alt: "..." },
+                    { src: "/assets/imgs/page/homepage1/13.png", alt: "..." },
+                    { src: "/assets/imgs/page/homepage1/14.png", alt: "..." },
+                    { src: "/assets/imgs/page/homepage1/15.png", alt: "..." },
+                    { src: "/assets/imgs/page/homepage1/16.png", alt: "..." },
+                    { src: "/assets/imgs/page/homepage1/17.png", alt: "..." },
+                    { src: "/assets/imgs/page/homepage1/18.png", alt: "..." },
+                    { src: "/assets/imgs/page/homepage1/19.png", alt: "..." },
+                    { src: "/assets/imgs/page/homepage1/20.png", alt: "..." }
+                ]
+                    .sort(() => Math.random() - 0.5)
+                    .map((logo, idx) => (
+                        <li key={idx} className="carouselTicker__item">
+                            <div className="item-logo">
+                                <img src={logo.src} alt={logo.alt} />
+                            </div>
+                        </li>
+                    ))}
+
+            </Marquee>
         </>
     )
 }
