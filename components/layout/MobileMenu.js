804feb0d8d0f0949e4743e8cda626c7902c17f8a
ELGhifari11 <ghifariakun@gmail.com>
2025-11-16 20:17:47 +0700

    feat(layout): update mobile menu with new links and dark icons
    - Add new menu items for Bootcamp, Double Pro, Services, Faq, and Find Us
    - Replace social media icons with dark versions
    - Remove commented out code and unused menu items

--- components/layout/MobileMenu.js
+++ components/layout/MobileMenu.js
@@ -46,7 +46,9 @@ export default function MobileMenu({ isMobileMenu, handleMobileMenu }) {
                                     <ul className="mobile-menu font-heading">
                                         <li className={isActive.key == 1 ? "has-children active" : "has-children"}>
                                             <span className="menu-expand" onClick={() => handleToggle(1)}></span>
-                                            <Link className="active" href="/">Home</Link>
+
+                                            <Link  onClick={() => handleMobileMenu()} href="/">Home</Link>
+
                                             {/* <ul className="sub-menu" style={{ display: `${isActive.key == 1 ? "block" : "none"}` }}>
                                                 <li><Link href="/">Business Solutions</Link></li>
                                                 <li><Link href="/index-2">Marketing App</Link></li>
@@ -56,23 +58,30 @@ export default function MobileMenu({ isMobileMenu, handleMobileMenu }) {
                                                 <li><Link href="/index-6">AI Platform</Link></li>
                                             </ul> */}
                                         </li>
+
+                                        <li><Link  onClick={() => handleMobileMenu()} href="#bootcamp">Bootcamp</Link></li>
+                                        <li><Link  onClick={() => handleMobileMenu()} href="#project-product">Double Pro</Link></li>
+                                        <li><Link  onClick={() => handleMobileMenu()} href="#services">Services</Link></li>
+                                        <li><Link  onClick={() => handleMobileMenu()} href="#faq">Faq</Link></li>
+                                        <li><Link  onClick={() => handleMobileMenu()} href="#find-us">Find Us</Link></li>
+
                                         <li className={isActive.key == 2 ? "has-children active" : "has-children"}>
-                                            <span className="menu-expand" onClick={() => handleToggle(2)}><i className="fi-rr-angle-small-down" /></span>
+                                            {/* <span className="menu-expand" onClick={() => handleToggle(2)}><i className="fi-rr-angle-small-down" /></span>
 
                                             <Link href="#">Inner Pages</Link>
                                             <ul className="sub-menu" style={{ display: `${isActive.key == 2 ? "block" : "none"}` }}>
                                                 <li><Link href="/about">About Us</Link></li>
                                                 <li><Link href="/service">Our Services</Link></li>
-                                                {/* <li><Link href="/pricing">Pricing Plan</Link></li>
+                                                <li><Link href="/pricing">Pricing Plan</Link></li>
                                                 <li><Link href="/help">Help Center</Link></li>
                                                 <li><Link href="/integration">Integrations</Link></li>
                                                 <li><Link href="/careers">Careers</Link></li>
                                                 <li><Link href="/job-detail">Job details</Link></li>
                                                 <li><Link href="/feature">Features V1</Link></li>
                                                 <li><Link href="/feature-2">Features V2</Link></li>
-                                                <li><Link href="/feature-3">Features V3</Link></li> */}
+                                                <li><Link href="/feature-3">Features V3</Link></li>
                                                 <li><Link href="/contact">Contact us</Link></li>
-                                            </ul>
+                                            </ul> */}
                                         </li>
                                         {/* <li className={isActive.key == 3 ? "has-children active" : "has-children"}>
                                             <span className="menu-expand" onClick={() => handleToggle(3)}><i className="fi-rr-angle-small-down" /></span>
@@ -117,22 +126,22 @@ export default function MobileMenu({ isMobileMenu, handleMobileMenu }) {
                                
                                 <h6 className="mb-25">Follow Us</h6>
                                 <Link className="icon-socials icon-facebook" href="https://www.facebook.com/pondokprogrammer">
-                                    <img alt="Pondok Programmer" style={{ width: '20px', height: '20px' }} src="/assets/imgs/template/icons/fb.svg" />
+                                    <img alt="Pondok Programmer" style={{ width: '20px', height: '20px' }} src="/assets/imgs/template/icons/fb-dark.svg" />
                                 </Link>
                                 <Link className="icon-socials icon-instagram" href="https://www.instagram.com/pondokprogrammer">
-                                    <img alt="Pondok Programmer" style={{ width: '20px', height: '20px' }} src="/assets/imgs/template/icons/ig.svg" />
+                                    <img alt="Pondok Programmer" style={{ width: '20px', height: '20px' }} src="/assets/imgs/template/icons/ig-dark.svg" />
                                 </Link>
                                 <Link className="icon-socials icon-github" href="https://github.com/pondokprogrammer">
-                                    <img alt="Pondok Programmer" style={{ width: '25px', height: '25px' }} src="/assets/imgs/template/icons/git.svg" />
+                                    <img alt="Pondok Programmer" style={{ width: '25px', height: '25px' }} src="/assets/imgs/template/icons/git-dark.svg" />
                                 </Link>
                                 <Link className="icon-socials icon-youtube" href="https://www.youtube.com/pondokprogrammer">
-                                    <img alt="Pondok Programmer" style={{ width: '20px', height: '20px' }} src="/assets/imgs/template/icons/yt.svg" />
-                                </Link>
-                                <Link className="icon-socials icon-telegram" href="https://t.me/pondokprogrammer">
-                                    <img alt="Pondok Programmer" style={{ width: '20px', height: '20px' }} src="/assets/imgs/template/icons/tel.svg" />
+                                    <img alt="Pondok Programmer" style={{ width: '20px', height: '20px' }} src="/assets/imgs/template/icons/yt-dark.svg" />
                                 </Link>
                                 <Link className="icon-socials icon-linkedin" href="https://www.linkedin.com/company/pondokprogrammer">
-                                    <img alt="Pondok Programmer" style={{ width: '20px', height: '20px' }} src="/assets/imgs/template/icons/in.svg" />
+                                    <img alt="Pondok Programmer" style={{ width: '20px', height: '20px' }} src="/assets/imgs/template/icons/in-dark.svg" />
+                                </Link>
+                                <Link className="icon-socials icon-tiktok" href="https://www.tiktok.com/@pondokprogrammer">
+                                    <img alt="Pondok Programmer" style={{ width: '20px', height: '20px' }} src="/assets/imgs/template/icons/tik-dark.svg" />
                                 </Link>
                                 <p/>
                             </div>
